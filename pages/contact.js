3e0f66e7921d70266790978997906b78202b8cdf
Elias Flores <elias.leo.flores@gmail.com>
2023-02-27 21:11:02 -0300

    form_terminado

---
+++ pages/contact.js
@@ -0,0 +1,27 @@
+let formulario = document.getElementById("form");
+let mensaje = document.getElementById("subject");
+
+/*DECLARACION DE FUNCION*/
+
+formulario = document.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+
+    if(mensaje.value != ""){
+
+        Swal.fire({
+            icon: 'succes',
+            text: 'Formulario Enviado',
+            
+          })
+        
+
+
+    }
+    else{
+        Swal.fire({
+            icon: 'warning',
+            text: 'Completa la consulta',
+            
+        })
+    }});
