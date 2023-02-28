fd43c74e67654a2c54bee775baf6bf35f82abdf6
Elias Flores <elias.leo.flores@gmail.com>
2023-02-28 11:54:09 -0300

    probando formulario

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,19 @@
 
 let formulario = document.getElementById("form");
 let mensaje = document.getElementById("subject");
+let mail = document.getElementById("email");
+/*DECLARACION DE ARRAY */
+
+let consultas = []
+
+/* DECLARACION DE OBJETO*/
+
+class Informes{
+    constructor(mail, consulta){
+        this.mail = mail;
+        this.consulta = consulta;
+    }
+}
 
 /*DECLARACION DE EVENTO*/
 
@@ -10,6 +23,12 @@ formulario = document.addEventListener("submit", (e) =>{
 
 /*AGREGO UN CONDICIONAL AL INPUT CON UN SWEET ALERT */
     if(mensaje.value != ""){
+
+        let nuevaConsulta = new Informes(mail.value, mensaje.value);
+        consultas.push(nuevaConsulta);
+
+        let arreglo_JSON = JSON.stringify(consultas);
+        localStorage.setItem("consultas", arreglo_JSON);
 
         Swal.fire({
             icon: 'success',
